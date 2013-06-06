d2b46628922b8b79a2283994adc41dc1b30c0e25
ino-shizuka <ino_shizuka@cygames.co.jp>
2013-06-06 18:41:12 +0900

    controller

--- fuel/app/classes/controller/regetataki.php
+++ fuel/app/classes/controller/regetataki.php
@@ -1,6 +1,5 @@
 <?php 
-class Controller_Regetataki extends Controller
-{
+class Controller_Regetataki extends Controller{
 	public function action_index(){
 		return View::forge('regetataki/top');
 	}
